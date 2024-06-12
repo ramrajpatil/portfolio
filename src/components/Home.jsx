ddbc117015af89f68df7e98a333bcedbed9ecf39
Ramraj Patil <ramrajpatil24@gmail.com>
2024-06-12 16:47:44 +0530

    restored home settings

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,54 @@
-// Home.js
 import React, { useEffect, useRef } from "react";
 import pdf from "../pdf/resume_ramraj.pdf";
 import hero from "./Data/hero.json";
 import Typed from "typed.js";
 
+
 const Home = () => {
-  const typedRefresh = useRef(null);
 
-  useEffect(() => {
+
+ const typedRefresh =  useRef(null);
+
+  useEffect(()=>{
     const options = {
-      strings: ["Hello, I am Ramraj Patil"],
-      typeSpeed: 50,
-      backSpeed: 50,
-      loop: true,
-    };
-    const typed = new Typed(typedRefresh.current, options);
+      strings:["Hello, I am Ramraj Patil"],
+      typeSpeed:50,
+      backSpeed:50,
+      loop:true
 
-    return () => {
+      }
+    const typed = new Typed(typedRefresh.current, options)
+
+    return () =>{
       typed.destroy();
-    };
-  }, []);
+    }
+
+  },[])
+
 
   return (
     <div className="container home mt-4" id="home">
-      <div className="content-wrapper" data-aos="fade-right" data-aos-duration="1000">
-        <div className="left">
-          <h1 ref={typedRefresh}></h1>
-          <p>A Full Stack Java Developer</p>
-          <a
-            href={pdf}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-outline-warning"
-          >
-            Download Resume
-          </a>
-        </div>
-        <div className="right">
-          <div className="img">
-            <img src={`/assets/${hero.imgSrc}`} alt="ramraj patil" />
-          </div>
+      <div className="left"
+      data-aos="fade-right"
+      data-aos-duration="1000"
+      >
+        <h1 ref={typedRefresh}></h1>
+        <p>A Full Stack Java Developer</p>
+        <a
+          href={pdf}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-outline-warning"
+        >
+          Download Resume
+        </a>
+      </div>
+      <div className="right"
+      data-aos="fade-left"
+      data-aos-duration="1000"
+      >
+        <div className="img">
+          <img src={`/assets/${hero.imgSrc}`} alt="ramraj patil" />
         </div>
       </div>
     </div>
